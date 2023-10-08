de5fabdd73b14feb9843ac209471ca36f3eaf26e
Madhav Kumar <lakshy1523@gmail.com>
2023-10-08 13:26:17 +0530

    Setting up the repo

--- utils/mypath.py
+++ utils/mypath.py
@@ -12,16 +12,16 @@ class MyPath(object):
         assert(database in db_names)
 
         if database == 'cifar-10':
-            return '/path/to/cifar-10/'
+            return './cifar-10/'
         
         elif database == 'cifar-20':
-            return '/path/to/cifar-20/'
+            return './cifar-20/'
 
         elif database == 'stl-10':
-            return '/path/to/stl-10/'
+            return './stl-10/'
         
         elif database in ['imagenet', 'imagenet_50', 'imagenet_100', 'imagenet_200']:
-            return '/path/to/imagenet/'
+            return './imagenet/'
         
         else:
             raise NotImplementedError
